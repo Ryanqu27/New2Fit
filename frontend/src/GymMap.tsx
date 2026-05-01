3bf2cebb9438bf9447cf02253b2be1287b109315
Ryan Qu <130172906+Ryanqu27@users.noreply.github.com>
2026-05-01 00:53:53 -0400

    Create api for sending gym location information. Set up leaflet to display gyms on a map for the frontend

---
+++ frontend/src/GymMap.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
