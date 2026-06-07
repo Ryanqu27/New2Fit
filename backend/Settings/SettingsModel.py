668bb181c40824cfe4bf2090c42bed91d3d2f56c
Ryan Qu <130172906+Ryanqu27@users.noreply.github.com>
2026-06-07 00:24:58 -0400

    Adjust CSS file for settings to match other heading and fix default value of theme to dark to correctly match default theme and options

--- backend/Settings/SettingsModel.py
+++ backend/Settings/SettingsModel.py
@@ -8,7 +8,7 @@ class UserSettings(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), unique=True, nullable=False)
     
-    theme = Column(String, default="system")
+    theme = Column(String, default="dark")
     unit_preference = Column(String, default="imperial")
     camera_framerate_preference = Column(Integer, default=30)
     workout_reminders = Column(Boolean, default=True)
